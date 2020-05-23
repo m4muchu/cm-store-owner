14a8de9390ba28d94ba8908f9ff067eae3766905
m4muchu <muchu.pkd123@gmail.com>
2020-05-23 18:05:33 +0530

    Product listing completed

--- src/js/routes/Admin/portal.ts
+++ src/js/routes/Admin/portal.ts
@@ -1,6 +1,11 @@
 import { Dashboard } from "js/components/admin/components/dasboard/dashboard";
+import { Products } from 'js/components/admin/components/catalogs/products';
 
 export const portalRoutes = [
+    {
+        path: "/admin/products",
+        component: Products
+    },
     {
         path: "/",
         component: Dashboard
