c8fe381c816204da13ef0362d9ff7e7239fb2aac
m4muchu <muchu.pkd123@gmail.com>
2020-06-15 09:19:07 +0530

    auth markup completed

---
+++ src/js/components/auth/layouts/auth-layout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import { AuthRoutes } from "js/routes/Auth/auth";
+
+
+
+const switchRoutes = (
+    <Switch>
+        {AuthRoutes.map((prop, key) => {
+            return <Route path={prop.path} component={prop.component} key={key} />;
+        })}
+    </Switch>
+);
+
+export const AuthLayout = () => {
+    return (
+        <React.Fragment>
+            <div className="auth-wrap">
+                {switchRoutes}
+            </div>
+        </React.Fragment>
+    )
+}
