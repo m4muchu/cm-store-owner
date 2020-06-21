f7a905c40e8b0926c1eb2734ac1bbf3295ddc2ef
m4muchu <muchu.pkd123@gmail.com>
2020-06-21 19:40:02 +0530

    converted to tsx

--- src/js/components/common/index.js
+++ src/js/components/common/index.js
@@ -1,6 +1,1 @@
-
-export * from './card.jsx';
-export * from './dashboard.jsx';
-export * from './mini_report.jsx';
-export * from './Top_products.jsx';
-export * from './activity.jsx';
+export * from './filter-selector';
