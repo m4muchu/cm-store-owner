5885a970966493f52fb22840d2275d9d67e8f23d
m4muchu <muchu.pkd123@gmail.com>
2020-07-26 10:25:31 +0530

    partial commit

--- src/js/components/auth/components/login/login.tsx
+++ src/js/components/auth/components/login/login.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
 import { history } from 'js/helpers';
+import { authServices } from 'js/services'
+import { configConstants } from 'js/constants';
 
 
 export const Login = () => {
+
+    const [ state, setState ] = useState({
+        email: '',
+        password: '',
+    });
+
+    interface loginResponse {
+        email: string,
+        access_token: string
+    }
+
+    const onSubmit = () => {
+        authServices.login(state).then((response: loginResponse) => {
+            localStorage.setItem(configConstants.ADMIN_TOKEN, response.access_token);
+            history.push('/admin/dashboard');
+        });
+    }
+
     return (
         <div className="container cm-admin-card">
             <Row>
@@ -11,9 +31,23 @@ export const Login = () => {
                     <h3>Sign In</h3>
                     <p>Sign in on the internal platform</p>
                     <div className="input-area">
-                        <Form.Control type="email" placeholder="Email" />
-                        <Form.Control type="password" placeholder="Password" />
-                        <Button type="submit" className="auth-submit-button" onClick={() => history.push('/admin/dashoard')}>LOG IN</Button>
+                        <Form.Control 
+                            type="email" 
+                            placeholder="Email" 
+                            onChange={(e) => setState({
+                                ...state,
+                                email: e.target.value
+                            })}
+                        />
+                        <Form.Control 
+                            type="password" 
+                            placeholder="Password" 
+                            onChange={(e) => setState({
+                                ...state,
+                                password: e.target.value
+                            })}
+                        />
+                        <Button type="submit" className="auth-submit-button" onClick={onSubmit}>LOG IN</Button>
                     </div>
                     <p className="forgot-password">Forgot Password ?</p>
                 </Col>
---
+++ src/js/constants/config-constants.ts
@@ -0,0 +1,7 @@
+interface configConstantTypes {
+    ADMIN_TOKEN: string
+}
+
+export const configConstants: configConstantTypes = {
+    ADMIN_TOKEN: 'admin_token'
+}
